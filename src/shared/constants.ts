8d8f5db40bb86e066bf57e46265bf2926eb1f419
mespinasso <matheus.espinasso@gmail.com>
2017-12-17 15:12:47 -0200

    Final ajustments + signed APK

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,5 +1,5 @@
-const igdbAPILocation = 'https://api-2445582011268.apicast.io/';
-// const igdbAPILocation = '/api';
+// const igdbAPILocation = 'https://api-2445582011268.apicast.io/';
+const igdbAPILocation = '/api';
 
 export const igdbUserKey = '069933f548c0ad99679d66341beb756b';
 export const igdbAPIRequests = {
